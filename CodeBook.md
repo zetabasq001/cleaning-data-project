5c75a1640ea1f04535c008adcffd756eddaf5cd4
zetabasq001 <basq001@yahoo.com>
2014-05-24 12:46:15 -0400

    add codebook(incomplete) & R-script

---
+++ CodeBook.md
@@ -0,0 +1,17 @@
+CodeBook
+========================================================
+
+**Objective of CodeBook**
+This is a Codebook describing the variables, data, and transformations that 
+the author applied to complete the Course Project for *Getting
+and Cleaning Data,* a MOOC presented by John Hopkins University via Coursera Platform.
+
+**Description of Raw Data**
+The data was collected via a wearable device with smartphones while 30 subjects
+engaged in six physical activites. 561 measurements were collected
+during these six activities.
+
+Data Set Reference: *Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012*
+
+**How Data was Manipulated and Transformed**
+incomplete
---
+++ run_analysis.R
@@ -0,0 +1,98 @@
+# the URL of file to download
+theURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+# name of destination file
+destfile <- "wearComp.zip"
+
+# command to download zip file from internet to destination file on local computer
+download.file(theURL, destfile)
+
+# unzip downloaded zip file
+unzip(destfile)
+
+# sets working directory to unzipped folder
+setwd("./UCI HAR Dataset")
+
+# reads features.txt file from UCI HAR Dataset directory
+# contains labels of 561 measurement variables
+features <- scan(file="features.txt", what=character(), sep="\n")
+
+# set working directory to test directory within previous directory
+setwd("./test")
+
+# read all three text files in test directory: subjects, data, & activities
+test_files <- list.files()[-1]
+test_data <- lapply(test_files, scan)
+
+# change working directory to train directory
+setwd("../train")
+
+# read all three text files in train directory: subjects, data, & activities
+train_files <- list.files()[-1]
+train_data <- lapply(train_files, scan)
+
+# finds vector position of all feature variables with mean in variable label
+mean_df_columns <- grep("mean", features, ignore.case=TRUE)
+
+# finds vector position of all feature variables with std in variable label
+std_df_columns <- grep("std", features, ignore.case=TRUE)
+
+# tidy up features variables: first extract out means and standard deviation columns
+features <- features[c(mean_df_columns, std_df_columns)]
+
+# second, string split and remove the numbers from label columns
+features <- strsplit(features, split=" ")
+features <- lapply(features, function(x) x <- x[2])
+
+# third, replace all shortnames with their respective longnames
+features <- lapply(features, function(x) gsub("Gyro", "Gryoscope", x))
+features <- lapply(features, function(x) gsub("Acc","Accelerometer", x)) 
+features <- lapply(features, function(x) gsub("-mean()", " Mean", x))
+features <- lapply(features, function(x) gsub("-std()", " Standard Deviation", x))
+features <- unlist(features)
+
+# replace activity nominal numbers by activity names by
+# first combining nominal activity numbers from test and train sets
+act_nominals <- c(test_data[[3]], train_data[[3]])
+
+# second, reading in activity labels corresponding to nominal numbers
+activity_labels <- scan("../activity_labels.txt", character(), sep="\n")
+
+# following two lines tidies up activity labels by
+# splitting string and removing front number from activity label 
+activity_labels <- lapply(activity_labels, function(x) x <- strsplit(x, split=" "))
+activity_labels <- lapply(activity_labels, function(x) x <- unlist(x)[-1])
+
+# substitute activity nominal numbers by corresponding descriptive label words
+activity_labels <- list(unlist(lapply(act_nominals, function(x) x <- activity_labels[x])))
+
+# list with one component: all test and train subjects represented nominally
+subjects_all <- list(c(test_data[[1]], train_data[[1]]))
+
+# created matrix by row with test set, & train set;
+# and extracted out mean and standard deviation columns and reconverted to data frame
+df_Sets <- as.data.frame(matrix(c(test_data[[2]], train_data[[2]]), ncol=561, byrow=TRUE)[,c(mean_df_columns, std_df_columns)])
+
+# names previous data frame with corresponding feature variable class labels 
+names(df_Sets) <- features
+
+# constructs last piece of the data frame corresponding to columns
+# subjects and activities
+df_subj_act <- data.frame(subjects_all, activity_labels)
+
+# name the previous data frame columns: Subjects and Activities
+names(df_subj_act) <- c("Subjects", "Activities") 
+
+# combines both pieces of previous data frames to complete the tidy data frame
+df_to_Pt4 <- data.frame(df_subj_act, df_Sets)
+
+# Step 5:
+library(reshape2)
+ls_of_melts <- lapply(3:88, function(x) melt(df_to_Pt4, id.vars=c("Subjects","Activities"), measure.vars=x, variable.name="Feature.Variable", value.name="Feature.Values"))
+ls_of_dcast <- lapply(ls_of_melts, function(x) dcast(x, Subjects~Activities, mean))
+ls_of_melts2 <- lapply(ls_of_dcast, function(x) melt(x, id.vars="Subjects", variable.name="Activities"))
+df_final1 <- ls_of_melts2[[1]][c(1,2)]
+df_final2 <- lapply(ls_of_melts2, function(x) x <- as.data.frame(x[c(-1,-2)]))
+df_Final <- data.frame(df_final1, df_final2)
+names(df_Final)[3:88] <- names(df_to_Pt4[,3:88])
+write.table(df_Final, "../df_Final.txt", sep=",")
